51c1e105111b543c40697d804f08137008af565d
Pallavi7597 <66312599+Pallavi7597@users.noreply.github.com>
2023-07-27 15:34:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,29 @@
-function calculateShipping() {
-    const weight = parseFloat(document.getElementById("weight").value);
-    const country = document.getElementById("country").value;
-    const pincode = document.getElementById("pincode").value;
-    const shipmentValue = parseFloat(document.getElementById("shipmentValue").value);
-   
-       // Define shipping rates based on weight and destination country
-       const rates = {
-           "USA": {
-               0.5: 5.0,
-               1: 7.5,
-               2: 10.0,
-               5: 15.0,
-               10: 20.0
-           },
-           "Canada": {
-               0.5: 6.0,
-               1: 8.5,
-               2: 12.0,
-               5: 17.0,
-               10: 22.0
-           },
-           "UK": {
-               0.5: 8.0,
-               1: 11.0,
-               2: 15.0,
-               5: 20.0,
-               10: 25.0
-           }
-        // Add more rates for other countries as needed
-       };
-   
-       let shippingCost = 0;
-       for (const rateWeight in rates[country]) {
-           if (weight <= rateWeight) {
-               shippingCost = rates[country][rateWeight];
-               break;
-           }
-       }
-   
-       // Additional shipping cost based on pin code (customize as needed)
-       if (country === "India") {
-           if (pincode.startsWith("50")) {
-               shippingCost += 5.0;
-           } else {
-               shippingCost += 8.0;
-           }
-       }
-   
-       // Additional shipping cost based on shipment value (customize as needed)
-       if (shipmentValue > 100) {
-           shippingCost += 2.0;
-       }
-   
-       const resultElement = document.getElementById("result");
-       resultElement.textContent = `Shipping Cost to ${country} (${pincode}) with shipment value $${shipmentValue.toFixed(2)}: $${shippingCost.toFixed(2)}`;
-   }
-   function resetForm() {
-       // Clear all input fields and result
-       document.getElementById("weight").value = "";
-       document.getElementById("country").selectedIndex = 0;
-       document.getElementById("pincode").value = "";
-       document.getElementById("shipmentValue").value = "";
-       document.getElementById("result").textContent = "";
-   }
+function calculateShipmentCost() {
+    const country = document.getElementById('country').value;
+    const weight = parseFloat(document.getElementById('weight').value);
+    const pickupPincode = document.getElementById('pickup').value;
+    const deliveryPincode = document.getElementById('delivery').value;
+    const distance = parseFloat(document.getElementById('distance').value);
+    const shipmentValue = parseFloat(document.getElementById('shipmentValue').value);
+  
+    // Assuming the shipment cost calculation is based on the distance, you can apply your own formula here.
+    // For demonstration purposes, we will use a simple fixed cost per kilometer.
+  
+    const costPerKilometer = 0.1; // Replace with your actual cost per kilometer
+  
+    const shipmentCost = distance * costPerKilometer;
+  
+    // You can add more complex calculations based on weight, country, shipment value, etc.
+  
+    const resultElement = document.getElementById('result');
+    const result = `Shipment Cost for ${country}:
+                    Weight: ${weight} kg
+                    Pickup Pincode: ${pickupPincode}
+                    Delivery Pincode: ${deliveryPincode}
+                    Distance: ${distance} km
+                    Shipment Value: $${shipmentValue}
+                    Calculated Shipment Cost: $${shipmentCost.toFixed(2)}`;
+  
+    resultElement.textContent = result;
+  }
+  
